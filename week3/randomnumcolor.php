a63855fc60e7bd1c176c82d786a340c7fc4ffa96
Annie Chan <annie212014@dasein.edu.my>
2022-10-23 20:54:49 +0800

    color

--- week3/randomnumcolor.php
+++ week3/randomnumcolor.php
@@ -4,21 +4,42 @@
 
 <head>
     <style>
-
+        .bigger {
+            font-weight: bold;
+        }
     </style>
 </head>
 
 <body>
+    <div class="container">
+        <div class="row text-center mt-5">
+            <div class="col p-3 bg-success">
+                <?php
+                $x = rand(5, 50);
+                $y = rand(5, 50);
 
-    <?php
-    $t = 10;
-    if ($t > 5) {
-        echo "Have a good day";
-    }
-
-
-    ?>
-
+                if ($x > $y) {
+                    echo "<span class=\"bigger\">";
+                    echo $x;
+                    echo "</span>";
+                } else {
+                    echo $x;
+                }
+                ?>
+            </div>
+            <div class="col p-3 bg-danger">
+                <?php
+                if ($x < $y) {
+                    echo "<span class=\"bigger\">";
+                    echo $y;
+                    echo "</span>";
+                } else {
+                    echo $y;
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
