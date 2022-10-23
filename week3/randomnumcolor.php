85877c1ac4c77e1220aa20810d466fc0dd278755
Annie Chan <annie212014@dasein.edu.my>
2022-10-23 18:35:39 +0800

    randomnumcolor php

---
+++ week3/randomnumcolor.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <style>
+
+    </style>
+</head>
+
+<body>
+
+    <?php
+    $t = 10;
+    if ($t > 5) {
+        echo "Have a good day";
+    }
+
+
+    ?>
+
+</body>
+
+</html>
