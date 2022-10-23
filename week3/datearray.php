f14a60c3c91701d074f96df66243fde1f5d8c562
Annie Chan <annie212014@dasein.edu.my>
2022-10-23 23:56:55 +0800

    week3 done

--- week3/datearray.php
+++ week3/datearray.php
@@ -16,12 +16,12 @@
 </head>
 
 <body>
-
     <?php
-    $selected_day = date('d'); //current day
+    $month = array("January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
+    $selected_day = date('d'); //current day
     echo "<div class=\"container text-center\">";
-    echo "<div class=\"btn-group p-5 gap-5\">";
+    echo "<div class=\"btn-group pt-3 gap-5\">";
     echo "<button class=\"btn btn-info btn-lg dropdown-toggle\" type=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">";
     echo "Day";
     echo "</button>";
@@ -35,17 +35,22 @@
     ?>
 
     <?php
-    $selected_month = date('m'); //current month
-
     echo "<button class=\"btn btn-warning btn-lg dropdown-toggle\" type=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">";
     echo "Month";
     echo "</button>";
     echo "<ul class=\"dropdown-menu scrollable-menu\">";
-    for ($i = 1; $i <= 12; $i++) {
-    ?>
-        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-    <?php
-    }
+    echo $month[0] . "<br>";
+    echo $month[1] . "<br>";
+    echo $month[2] . "<br>";
+    echo $month[3] . "<br>";
+    echo $month[4] . "<br>";
+    echo $month[5] . "<br>";
+    echo $month[6] . "<br>";
+    echo $month[7] . "<br>";
+    echo $month[8] . "<br>";
+    echo $month[9] . "<br>";
+    echo $month[10] . "<br>";
+    echo $month[11] . "<br>";
     echo "</ul>";
     ?>
 
@@ -65,7 +70,6 @@
     echo '</select>' . "\n";
     echo "</ul>";
     ?>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
